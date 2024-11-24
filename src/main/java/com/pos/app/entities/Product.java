370445588a476a343916717d4daced231801f078
rivo <rivopelu12@gmail.com>
2024-11-25 01:12:06 +0800

    FEAT:add table product

---
+++ src/main/java/com/pos/app/entities/Product.java
@@ -0,0 +1,64 @@
+package com.pos.app.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigInteger;
+import java.util.Date;
+import java.util.UUID;
+
+@Data
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "product")
+public class Product {
+    @Id
+    private String id;
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "price")
+    private BigInteger price;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "client_id")
+    private String clientId;
+
+    @Column(name = "created_date")
+    private Long createdDate;
+
+    @Column(name = "updated_date")
+    private Long updatedDate;
+
+    @Column(name = "created_by")
+    private String createdBy;
+
+    @Column(name = "updated_by")
+    private String updatedBy;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+
+    @PrePersist
+    public void prePersist() {
+        if (this.id == null) {
+            this.id = UUID.randomUUID().toString();
+        }
+        this.createdDate = new Date().getTime();
+        this.updatedDate = new Date().getTime();
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        this.updatedDate = new Date().getTime();
+    }
+}
